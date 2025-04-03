56803cf5f035f354f1d4f43db16d3d4a6b157d44
warzes <warzes@mail.ru>
2025-04-03 14:27:45 +1000

    - код сцены

---
+++ src/Game/Scene.cpp
@@ -0,0 +1,147 @@
+﻿#include "stdafx.h"
+#include "Scene.h"
+//=============================================================================
+Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
+	: m_position(position)
+	, m_front(glm::vec3(0.0f, 0.0f, -1.0f))
+	, m_up(up)
+	, m_right(glm::vec3(1.0f, 0.0f, 0.0f))
+	, m_worldUp(up)
+	, m_yaw(yaw)
+	, m_pitch(pitch)
+	, m_movementSpeed(SPEED)
+	, m_mouseSensitivity(SENSITIVITY)
+	, m_zoom(ZOOM)
+{
+	updateCameraVectors();
+}
+//=============================================================================
+glm::mat4 Camera::GetViewMatrix() const
+{
+	return glm::lookAt(m_position, m_position + m_front, m_up);
+}
+//=============================================================================
+glm::mat4 Camera::GetProjectionMatrix(float aspect) const
+{
+	return glm::perspective(glm::radians(m_zoom), aspect, 0.1f, 1000.0f);
+}
+//=============================================================================
+void Camera::ProcessMouseMovement(float xoffset, float yoffset)
+{
+	xoffset *= m_mouseSensitivity;
+	yoffset *= m_mouseSensitivity;
+
+	m_yaw += xoffset;
+	m_pitch += yoffset;
+
+	if (m_pitch > 89.0f) m_pitch = 89.0f;
+	if (m_pitch < -89.0f) m_pitch = -89.0f;
+
+	updateCameraVectors();
+}
+//=============================================================================
+void Camera::ProcessKeyboard(Direction direction, float deltaTime)
+{
+	float velocity = m_movementSpeed * deltaTime;
+	if (direction == Direction::Forward)  m_position += m_front * velocity;
+	if (direction == Direction::Backward) m_position -= m_front * velocity;
+	if (direction == Direction::Left)     m_position -= m_right * velocity;
+	if (direction == Direction::Right)    m_position += m_right * velocity;
+}
+//=============================================================================
+void Camera::updateCameraVectors()
+{
+	glm::vec3 front;
+	front.x = cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+	front.y = sin(glm::radians(m_pitch));
+	front.z = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+	m_front = glm::normalize(front);
+
+	m_right = glm::normalize(glm::cross(m_front, m_worldUp));
+	m_up = glm::normalize(glm::cross(m_right, m_front));
+}
+//=============================================================================
+void Scene::AddNode(Node* node)
+{
+	m_nodes.push_back(node);
+}
+//=============================================================================
+void Scene::Render(Shader& shader, Camera& camera, float screenAspect)
+{
+	glm::mat4 viewProjectionMatrix = camera.GetProjectionMatrix(screenAspect) * camera.GetViewMatrix();
+	shader.Bind();
+
+	for (auto node : m_nodes)
+	{
+		node->UpdateWorldMatrix();
+		if (isVisible(node, viewProjectionMatrix))
+		{	
+			glm::mat4 modelMatrix = node->GetWorldMatrix();
+			glProgramUniformMatrix4fv(shader.GetID(), m_modelLocation, 1, GL_FALSE, &modelMatrix[0][0]);
+			auto model = node->GetModel();
+			if (model) model->Draw();
+		}
+	}
+}
+//=============================================================================
+void Scene::SetModelLocation(unsigned int location)
+{
+	m_modelLocation = location;
+}
+//=============================================================================
+bool Scene::isVisible(Node* node, const glm::mat4& viewProjectionMatrix) const
+{
+	if (!node->GetModel()) return false;
+	return isSphereVisible(node, viewProjectionMatrix) || isAABBVisible(node, viewProjectionMatrix);
+}
+//=============================================================================
+bool Scene::isSphereVisible(Node* node, const glm::mat4& viewProjectionMatrix) const
+{
+	return true; // TODO: пока не работает isSphereVisible и isAABBVisible
+
+	// Предположим, что сфера охватывает всю модель
+	glm::vec3 center = node->GetTransform().GetPosition();
+	float radius = 1.0f; // Предположительный радиус модели
+
+	glm::vec4 sphereCenter = viewProjectionMatrix * glm::vec4(center, 1.0f);
+	if (std::abs(sphereCenter.x) > sphereCenter.w || std::abs(sphereCenter.y) > sphereCenter.w || std::abs(sphereCenter.z) > sphereCenter.w)
+	{
+		return false;
+	}
+	return true;
+}
+//=============================================================================
+bool Scene::isAABBVisible(Node* node, const glm::mat4& viewProjectionMatrix) const
+{
+	return true; // TODO: пока не работает isSphereVisible и isAABBVisible
+
+	// Предположим, что AABB охватывает всю модель
+	glm::vec3 min = glm::vec3(-1.0f, -1.0f, -1.0f); // Минимальные координаты модели
+	glm::vec3 max = glm::vec3(1.0f, 1.0f, 1.0f);   // Максимальные координаты модели
+
+	glm::mat4 worldMatrix = node->GetWorldMatrix();
+	glm::vec4 corners[] = 
+	{
+		worldMatrix * glm::vec4(min.x, min.y, min.z, 1.0f),
+		worldMatrix * glm::vec4(max.x, min.y, min.z, 1.0f),
+		worldMatrix * glm::vec4(min.x, max.y, min.z, 1.0f),
+		worldMatrix * glm::vec4(max.x, max.y, min.z, 1.0f),
+		worldMatrix * glm::vec4(min.x, min.y, max.z, 1.0f),
+		worldMatrix * glm::vec4(max.x, min.y, max.z, 1.0f),
+		worldMatrix * glm::vec4(min.x, max.y, max.z, 1.0f),
+		worldMatrix * glm::vec4(max.x, max.y, max.z, 1.0f)
+	};
+
+	for (auto& corner : corners)
+	{
+		glm::vec4 transformedCorner = viewProjectionMatrix * corner;
+		if (transformedCorner.x >= -transformedCorner.w && transformedCorner.x <= transformedCorner.w &&
+			transformedCorner.y >= -transformedCorner.w && transformedCorner.y <= transformedCorner.w &&
+			transformedCorner.z >= -transformedCorner.w && transformedCorner.z <= transformedCorner.w)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+//=============================================================================
---
+++ src/Game/Scene.h
@@ -0,0 +1,142 @@
+﻿#pragma once
+
+#include "Graphics.h"
+
+class Transform final
+{
+public:
+	Transform() = default;
+	glm::vec3 GetPosition() const { return m_position; }
+	glm::quat GetRotation() const { return m_rotation; }
+	glm::vec3 GetScale() const { return m_scale; }
+
+	void SetPosition(const glm::vec3& position) { m_position = position; }
+	void SetRotation(const glm::quat& rotation) { m_rotation = rotation; }
+	void SetScale(const glm::vec3& scale) { m_scale = scale; }
+
+	void Translate(const glm::vec3& translation) { m_position += translation; }
+
+	// Вращение вокруг своей оси
+	void Rotate(float angle, const glm::vec3& axis)
+	{
+		m_rotation = glm::normalize(glm::rotate(m_rotation, glm::radians(angle), axis));
+	}
+
+	// Вращение вокруг заданной точки
+	void RotateAroundPoint(float angle, const glm::vec3& axis, const glm::vec3& point)
+	{
+		glm::quat rot = glm::angleAxis(glm::radians(angle), axis);
+		m_position = point + rot * (m_position - point);
+		m_rotation = glm::normalize(rot * m_rotation);
+	}
+
+	void Scale(const glm::vec3& scale) { m_scale *= scale; }
+
+	glm::mat4 GetModelMatrix() const
+	{
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, m_position);
+		model = model * glm::toMat4(m_rotation);
+		model = glm::scale(model, m_scale);
+		return model;
+	}
+
+private:
+	glm::vec3 m_position = glm::vec3{ 0.0f };
+	glm::quat m_rotation = { 1.0f, 0.0f, 0.0f, 0.0f };
+	glm::vec3 m_scale = glm::vec3{ 1.0f };
+};
+
+class Node final
+{
+public:
+	Node() = default;
+
+	void SetModel(std::shared_ptr<Model> model) { m_model = model; }
+	std::shared_ptr<Model> GetModel() const { return m_model; }
+
+	void AddChild(Node* child) { m_children.push_back(child); }
+	const std::vector<Node*>& GetChildren() const { return m_children; }
+
+	// TODO: не учитывается трасформа от предка
+	Transform& GetTransform() { return m_transform; }
+	const Transform& GetTransform() const { return m_transform; }
+
+	glm::mat4 GetWorldMatrix() const
+	{
+		if (m_parent) return m_parent->GetWorldMatrix() * m_transform.GetModelMatrix();
+		else return m_transform.GetModelMatrix();
+	}
+
+	void UpdateWorldMatrix()
+	{
+		m_worldMatrix = GetWorldMatrix();
+		for (auto& child : m_children)
+		{
+			child->m_parent = this;
+			child->UpdateWorldMatrix();
+		}
+	}
+
+private:
+	Transform              m_transform;
+	std::shared_ptr<Model> m_model;
+	mutable glm::mat4      m_worldMatrix;
+	Node*                  m_parent = nullptr;
+	std::vector<Node*>     m_children;
+};
+
+enum class Direction : uint8_t
+{
+	Forward,
+	Backward,
+	Left,
+	Right
+};
+
+class Camera final
+{
+	const float SPEED = 2.5f;
+	const float SENSITIVITY = 0.1f;
+	const float ZOOM = 45.0f;
+public:
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.0f, float pitch = 0.0f);
+
+	glm::mat4 GetViewMatrix() const;
+	glm::mat4 GetProjectionMatrix(float aspect) const;
+
+	void ProcessMouseMovement(float xoffset, float yoffset);
+	void ProcessKeyboard(Direction direction, float deltaTime);
+
+private:
+	void updateCameraVectors();
+
+	glm::vec3 m_position;
+	glm::vec3 m_front;
+	glm::vec3 m_up;
+	glm::vec3 m_right;
+	glm::vec3 m_worldUp;
+	float m_yaw;
+	float m_pitch;
+	float m_movementSpeed;
+	float m_mouseSensitivity;
+	float m_zoom;
+};
+
+class Scene final 
+{
+public:
+	Scene() = default;
+
+	void AddNode(Node* node);
+	void Render(Shader& shader, Camera& camera, float screenAspect);
+	void SetModelLocation(unsigned int location);
+
+private:
+	bool isVisible(Node* node, const glm::mat4& viewProjectionMatrix) const;
+	bool isSphereVisible(Node* node, const glm::mat4& viewProjectionMatrix) const;
+	bool isAABBVisible(Node* node, const glm::mat4& viewProjectionMatrix) const;
+
+	std::vector<Node*> m_nodes;
+	unsigned int m_modelLocation;
+};
